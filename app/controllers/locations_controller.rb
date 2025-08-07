729c8bbf224a5e9e1374417af5b16ca772aecd9a
itagaki <itagaki_shun@vitallead.co.jp>
2025-08-07 16:41:06 +0900

    ストロングパラメータの設定、createメソッドを作成

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -7,4 +7,20 @@ class LocationsController < ApplicationController
   def new
     @locations = Location.new
   end
+
+  def create
+    @locations = Location.new(location_params)
+
+    if @location.save
+      redirect_to @location
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:longitude, :latitude)
+  end
 end
